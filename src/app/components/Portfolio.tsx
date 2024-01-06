b8e5d95627f07f30445668708187c44bda317580
Tiago Cambará <tc@learningbank.io>
2024-01-06 16:54:59 +0000

    add portfolio

---
+++ src/app/components/Portfolio.tsx
@@ -0,0 +1,9 @@
+
+const Portfolio:React.FC = () => {
+
+    return (
+        <iframe src="/assets/PORTFOLIO24_ANA_MORAES_ROCHA.pdf" width={"100%"} style={{border:0, height: "97vh"}}></iframe>
+    )
+}
+
+export default Portfolio;
